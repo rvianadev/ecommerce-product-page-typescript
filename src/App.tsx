d2937d94676147132416f7dc2fa136db3d135bf0
Rodrigo Viana <roviol@gmail.com>
2024-01-14 11:10:14 -0300

    feature: products list page (wip)

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import { Container, Header, ProductsList } from 'components';
 
 export function App() {
   const location = useLocation();
+  console.log(location);
 
   return (
     <Container>
       <Header />
       <div className="container w-product-container mx-auto flex justify-center">
-        {location.pathname === '/product/sneaker' ? null : <ProductsList />}
+        {location.pathname === '/' ? <ProductsList /> : null}
         {location.pathname === '/product/sneaker' ? <Outlet /> : null}
+        {location.pathname === '/product/:productId' ? <Outlet /> : null}
       </div>
     </Container>
   );
