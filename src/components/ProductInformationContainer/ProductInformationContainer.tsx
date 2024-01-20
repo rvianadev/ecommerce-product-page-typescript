da158c09395cfdaf99da5a51511ca40f42696146
Rodrigo <roviol@gmail.com>
2024-01-20 19:10:30 -0300

    amount

--- src/components/ProductInformationContainer/ProductInformationContainer.tsx
+++ src/components/ProductInformationContainer/ProductInformationContainer.tsx
@@ -1,8 +1,24 @@
+import { useState } from 'react';
+
 import iconMinus from '../../assets/images/icon-minus.svg';
 import iconPlus from '../../assets/images/icon-plus.svg';
 import iconCart from '../../assets/images/icon-cart.svg';
 
 export function ProductInformationContainer() {
+  const [amount, setAmount] = useState(0);
+
+  const increaseAmount = () => {
+    if (amount < 20) {
+      setAmount(amount + 1);
+    }
+  };
+
+  const decreaseAmount = () => {
+    if (amount > 0) {
+      setAmount(amount - 1);
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center w-2.2/5 max-w-[27.8125rem]">
       <div>
@@ -32,16 +48,35 @@ export function ProductInformationContainer() {
       <div className="flex justify-between">
         <div className="flex justify-around items-center rounded-[0.625rem] h-14 bg-[#F6F8FD] w-[35.28%] max-w-[9.8125rem]">
           <button>
-            <img src={iconMinus} alt="icon minus" />
+            <a
+              href="#"
+              onClick={() => {
+                decreaseAmount();
+              }}
+            >
+              <img src={iconMinus} alt="icon minus" />
+            </a>
           </button>
-          <span>0</span>
+          <span className="font-bold">{amount}</span>
           <button>
-            <img src={iconPlus} alt="icon plus" />
+            <a href="#" onClick={() => increaseAmount()}>
+              <img src={iconPlus} alt="icon plus" />
+            </a>
           </button>
         </div>
 
         <button className="flex justify-evenly items-center h-14 bg-primary rounded-[0.625rem] w-[61.12%] max-w-[17rem]">
-          <img src={iconCart} alt="icon cart" />
+          <svg
+            width="22"
+            height="20"
+            xmlns="http://www.w3.org/2000/svg"
+            className="fill-white"
+          >
+            <path
+              d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
+              fillRule="nonzero"
+            />
+          </svg>
           <span className="text-white font-bold">Add to cart</span>
         </button>
       </div>
